1b33df968503ca21412f4b5b11dbd76c58db69e9
Matthew <matthew.thompson.a@gmail.com>
2016-07-11 21:18:26 -0400

    Updated Chat Screen with Username function.

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -6,6 +6,52 @@ var form = document.getElementById('chatForm');
 var mes = document.getElementById('msg');
 var ul = document.getElementById('messages');
 
+var chatName = document.getElementById("screenName");
+var nameValue = chatName.value;
+    chatName.onchange = function() {
+       nameValue = chatName.value;
+     };
+
+function updateScroll(){
+    var ul = document.getElementById("messages");
+    ul.scrollTop = ul.scrollHeight;
+      // console.log(ul.scrollHeight + 100);
+}
+
+if(form.addEventListener){
+  form.addEventListener("submit",
+  function(evt){
+    evt.preventDefault();
+    socket.emit('chat message', nameValue + ": " + mes.value);
+    mes.value = '';
+    return false;
+  },
+    false)
+};
+
+
+socket.on('chat message', function(msg){
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(msg);
+  node.appendChild(textnode);
+  // <li> textnode </li>
+  ul.appendChild(node);
+  updateScroll();
+  // console.log('RAWR');
+  // console.log(nameValue);
+});
+
+// socket.on('chat message', function(msg){
+//   var node = document.createElement("li");
+//   var textnode = document.createTextNode(msg);
+//   node.appendChild(textnode);
+//   // <li> textnode </li>
+//   ul.appendChild(node);
+//   updateScroll();
+//   // console.log('RAWR');
+//   // console.log(nameValue);
+// });
+
 // $('form').submit(function(){
 //   socket.emit('chat message', $('#m').val());
 //   console.log($('#m').val());
@@ -16,31 +62,3 @@ var ul = document.getElementById('messages');
 // socket.on('chat message', function(msg){
 //  $('#messages').append($('<li>').text(msg));
 //  });
-
-
-function updateScroll(){
-    var ul = document.getElementById("messages");
-    ul.scrollTop = ul.scrollHeight;
-      console.log(ul.scrollHeight + 100);
-}
-
-console.log("loaded file");
-if(form.addEventListener){
-  form.addEventListener("submit",
-  function(evt){
-    evt.preventDefault();
-    socket.emit('chat message', mes.value);
-    mes.value = '';
-    return false;
-  },
-    false)
-};
-
-socket.on('chat message', function(msg){
-  var node = document.createElement("li");
-  var textnode = document.createTextNode(msg);
-  node.appendChild(textnode);
-  ul.appendChild(node);
-  updateScroll();
-  console.log('RAWR');
-});
